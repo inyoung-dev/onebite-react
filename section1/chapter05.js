49e359e4668fb733f32e8ba6dcbfcf4c0a332771
zero <inzero1998@gmail.com>
2025-04-30 09:01:46 +0900

    Chapter05

---
+++ section1/chapter05.js
@@ -0,0 +1,36 @@
+// 1. Number Type
+let num1 = 28;
+let num2 = 1.5;
+let num3 = -20;
+/*
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+*/
+
+let inf = Infinity;
+let mInf = -Infinity;
+
+let nan = NaN;
+// console.log(1 * "hello");
+
+// 2. String Type
+let myName = "Zero";
+let myLocation = "화성시"
+let introduce = myName + myLocation;
+
+let introduceText = `${myName}은 ${myLocation}에 거주합니다`; //  `${myName}은 ${myLocation}에 거주합니다` = 템플릿 리터럴 문법
+//console.log(introduceText);
+
+// 3. Boolean Type
+let isSwitchOn = true;
+let isEmpty = false;
+
+// 4. Null Type (아무것도 없다.)
+let empty = null;
+
+// 5. Undefined Type
+let none;
+console.log(none);
